f814006653979e4649c693fd6957fc624fd04344
Musie <mmengesha27@gmail.com>
2023-12-11 23:06:40 +0300

    fixing the ip

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['100.26.231.12', '54.197.101.47']
 
 
 def do_deploy(archive_path):
